358884915b34310a94b27aec4852246522c24db4
adem kalfalı <ademkalfa9@gmail.com>
2023-02-16 15:15:08 +0300

    Header-update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -10,10 +12,10 @@ function App() {
           <Route exact path="/">
             <Login />
           </Route>
-          {/* <Route path="/home">
+          <Route path="/home">
             <Header />
             <Home />
-          </Route> */}
+          </Route>
         </Switch>
       </Router>
     </div>
