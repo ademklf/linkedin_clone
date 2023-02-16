9acaae21abf65a3fbb0f0b8068e9d91eed0b0972
adem kalfalı <ademkalfa9@gmail.com>
2023-02-16 18:42:15 +0300

    header-update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,50 @@
 import styled from "styled-components";
 
 const Home = (props) => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Section>
+        <h5>
+          <a>Hiring in a hurry? -</a>
+          <p>
+            Find talented pros in a record time with Upwork and keep business
+            moving
+          </p>
+        </h5>
+      </Section>
+    </Container>
+  );
 };
+const Container = styled.div`
+  padding-top: 52px;
+  max-width: 100%;
+`;
+
+const Section = styled.section`
+  min-height: 50px;
+  padding: 16px 0;
+  box-sizing: content-box;
+  text-align: center;
+  text-decoration: underline;
+  display: flex;
+  justify-content: center;
+
+  h5 {
+    color: #0a66c2;
+    font-size: 14px;
+    a {
+      font-weight: 700;
+    }
+  }
+  p {
+    font-size: 14px;
+    color: #434696;
+    font-weight: 600;
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
 
 export default Home;
