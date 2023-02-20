511c4a582e2a42ed36bf379034c4bacfb9d54680
adem kalfalı <ademkalfa9@gmail.com>
2023-02-20 14:28:38 +0300

    main update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,61 @@
 import styled from "styled-components";
 import InsertPhotoOutlinedIcon from "@mui/icons-material/InsertPhotoOutlined";
+import OndemandVideoOutlinedIcon from "@mui/icons-material/OndemandVideoOutlined";
+import EventNoteOutlinedIcon from "@mui/icons-material/EventNoteOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 const Main = (props) => {
   return (
     <Container>
-      <ShareBox>Share</ShareBox>
-      <div>
-        <img src="/images/user.svg" alt="user.svg" />
-        <button>Start a post</button>
-      </div>
+      <ShareBox>
+        Share
+        <div>
+          <img src="/images/user.svg" alt="user.svg" />
+          <button>Start a post</button>
+        </div>
+        <div>
+          <button>
+            <InsertPhotoOutlinedIcon className="icon" />
+            <span>Photo</span>
+          </button>
+          <button>
+            <OndemandVideoOutlinedIcon />
+            <span>Video</span>
+          </button>
 
+          <button>
+            <EventNoteOutlinedIcon />
+            <span>Event</span>
+          </button>
+
+          <button>
+            <ArticleOutlinedIcon />
+            <span>Write article</span>
+          </button>
+        </div>
+      </ShareBox>
       <div>
-        <button>
-          <InsertPhotoOutlinedIcon className="icon" />
-        </button>
+        <Article>
+          <SharedActor>
+            <a>
+              <img src="/images/user.svg" alt="user" />
+              <div>
+                <span>Title</span>
+                <span>Info</span>
+                <span>Date</span>
+              </div>
+            </a>
+            <button>
+              <img src="/images/ellipsis.svg" alt="ellipsis" />
+            </button>
+          </SharedActor>
+          <Description>Description</Description>
+          <SharedImg>
+            <a>
+              <img src="/images/shared-image.jpg" alt="shared" />
+            </a>
+          </SharedImg>
+        </Article>
       </div>
     </Container>
   );
@@ -84,6 +127,80 @@ const ShareBox = styled(CommonCard)`
         }
       }
     }
+  }
+`;
+const Article = styled(CommonCard)`
+  padding: 0;
+  margin: 0 0 8px;
+  overflow: visible;
+`;
+
+const SharedActor = styled.div`
+  /* padding-right: 40px; */
+  flex-wrap: nowrap;
+  padding: 12px 16px 0;
+  margin-bottom: 8px;
+  align-items: center;
+  display: flex;
+  a {
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    text-decoration: none;
+    img {
+      width: 48px;
+      height: 48px;
+    }
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+        &:nth-child(n + 1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+  button {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+  }
+`;
+const Description = styled.div`
+  padding: 0 16px;
+  overflow: hidden;
+  color: rgba(0, 0, 0, 0.9);
+  font-size: 14px;
+  text-align: left;
+`;
+
+const SharedImg = styled.div`
+  margin-top: 8px;
+  width: 100%;
+  display: block;
+  position: relative;
+  background-color: #f9fafb;
+  img {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+    max-height: 400px;
   }
 `;
 
